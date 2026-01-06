37e8dc2e67699be5b1b5188928dad83ea1072044
shorav24oct <sharmasaurav142@gmail.com>
2026-01-06 08:18:07 +0530

    initial commit

---
+++ demo/src/main/java/com/example/controllers/MyController.java
@@ -0,0 +1,16 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@Controller
+@Service
+@Repository
+@RestController
+public class MyController {
+
+}
