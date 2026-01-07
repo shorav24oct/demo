ec56f8d200780fdb34e50b22de2d8ffdc21e8aaf
shorav24oct <sharmasaurav142@gmail.com>
2026-01-07 20:17:30 +0530

    add one more version of Student Service

---
+++ demo/src/main/java/com/example/services/StudentServiceImplV2.java
@@ -0,0 +1,25 @@
+package com.example.services;
+
+import com.example.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImplV2 implements StudentService{
+    @Override
+    public Student addStudent(Student student) {
+        return null;
+    }
+
+    @Override
+    public Optional<Student> getStudent(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Student> getStudents() {
+        return List.of();
+    }
+}
